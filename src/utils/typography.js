c1d8b5c6e89762e413218388815df0ff0f3f9bc3
sundvall <martin.sundvall@comeon.com>
2018-04-07 11:10:12 +0200

    add typography plugin to style code

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,10 @@
-import Typography from "typography";
-import themeSutro from "typography-theme-Sutro";
+/* eslint-disable import/no-extraneous-dependencies */
+import Typography from 'typography';
+import themeSutro from 'typography-theme-sutro';
+import CodePlugin from 'typography-plugin-code';
 
+// for elements of type 'code:
+themeSutro.plugins = [new CodePlugin()];
 /* test the connection of the css. Remove the 'layouts/index.css' that might exist
 and has precedence */
 // const typography = new Typography({ baseFontSize: "5px" });
